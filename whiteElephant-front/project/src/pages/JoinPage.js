7c0e853f8d0bb9164e79da5aa98a1b36ba142530
Jaehoon Ahn <ahnza245@gmail.com>
2024-08-25 00:46:23 +0900

    Set onChange event for each box

--- whiteElephant-front/project/src/pages/JoinPage.js
+++ whiteElephant-front/project/src/pages/JoinPage.js
@@ -60,36 +60,34 @@ const JoinPage = () => {
 		};
 
 	return (
-		<>
-			<div className="container">
-					<form className="form" onSubmit={handleSubmit}>
-						<h2 className="form-title">회원가입</h2>
-						<div className="form-group">
-							<label htmlFor="name">이름</label>
-							<input type="text" id="name" placeholder="Value" />
-						</div>
-						<div className="form-group">
-							<label htmlFor="email">이메일</label>
-							<input type="email" id="email" placeholder="Value" />
-						</div>
-						<div className="form-group">
-							<label htmlFor="password">비밀번호</label>
-							<input type="password" id="password" placeholder="Value" />
-						</div>
-						<div className="form-group">
-							<label htmlFor="confirm-password">비밀번호 확인</label>
-							<input type="password" id="confirm-password" placeholder="Value" />
-						</div>
-						<div className="form-group">
-							<label htmlFor="phone">전화번호</label>
-							<input type="tel" id="phone" placeholder="Value" />
-						</div>
-						<button type="submit" className="button">가입</button>
-						<Link to="/" className="link">로그인으로 돌아가기</Link>
-					</form>
-				</div>
-		</>
-	)
+        <div className="container">
+            <form className="form" onSubmit={handleSubmit}>
+                <h2 className="form-title">회원가입</h2>
+                <div className="form-group">
+                    <label htmlFor="name">이름</label>
+                    <input type="text" id="name" placeholder="Value" value={formData.name} onChange={handleChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">이메일</label>
+                    <input type="email" id="email" placeholder="Value" value={formData.email} onChange={handleChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">비밀번호</label>
+                    <input type="password" id="password" placeholder="Value" value={formData.password} onChange={handleChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="confirm-password">비밀번호 확인</label>
+                    <input type="password" id="confirm-password" placeholder="Value" value={formData.confirmPassword} onChange={handleChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="phone">전화번호</label>
+                    <input type="tel" id="phone" placeholder="Value" value={formData.phone} onChange={handleChange} />
+                </div>
+                <button type="submit" className="button">가입</button>
+                <Link to="/" className="link">로그인으로 돌아가기</Link>
+            </form>
+        </div>
+    );
 }
 
 export default JoinPage;
