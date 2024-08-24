7c4f1fc211e9339f9375d9138370cb794e59d1bd
iamgyu <iamgyu@naver.com>
2024-08-24 17:05:02 +0900

    Feat: 프로젝트 생성

---
+++ whiteElephant-front/project/src/pages/JoinPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const JoinPage = () => {
+    return (
+        <>
+            <p>회원가입 회원가입 회원가입</p>
+            <Link to="/">로그인</Link>
+        </>
+    )
+}
+
+export default JoinPage;
---
+++ whiteElephant-front/project/src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LoginPage = () => {
+    return (
+        <>
+            <p>로그인 로그인 로그인</p>
+            <Link to="join">회원가입</Link>
+        </>
+    )
+}
+
+export default LoginPage;
